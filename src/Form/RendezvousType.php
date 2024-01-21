9e393ad84621558e487c3562aa591404f7400f39
petitdekpe <petitdekpe@gmail.com>
2024-01-21 16:59:36 +0100

    contents updated

--- src/Form/RendezvousType.php
+++ src/Form/RendezvousType.php
@@ -47,7 +47,9 @@ class RendezvousType extends AbstractType
             'label' => 'Choisissez un créneau',
             'class' => Creneau::class,
             'choice_label' => 'libelle',
-            'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500']
+            'expanded' => true,
+            'multiple' => false,
+            'attr' => ['class' => '']
         ])
         ->add('image', VichImageType::class, [
             'label' => 'Une photo de vos mains / pieds',
