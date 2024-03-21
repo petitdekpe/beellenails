483f4620056b769437bf4babe5fe0788f78078a3
petitdekpe <petitdekpe@gmail.com>
2024-03-21 14:48:02 +0100

    le champ day est required dans le formulaire de prise de rdv + modification de la page de connexion

--- src/Form/RendezvousType.php
+++ src/Form/RendezvousType.php
@@ -27,10 +27,11 @@ class RendezvousType extends AbstractType
         ->add('day', DateType::class, [
                 'label' => 'Date du rendez-vous',
                 'placeholder' => 'Cliquez pour choisir une date de rendez-vous',
+                'required' => true,
                 'widget' => 'single_text',
                 'html5' => false,
                 'format' => 'yyyy-MM-dd',
-                'required' => true
+                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 'id' => 'datepicker'],
 
             // Vous pouvez également ajouter d'autres options de champ ici si nécessaire
         ])
