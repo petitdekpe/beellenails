7dcba978f9ee21a9d501a43636c533fff1a18ad9
petitdekpe <petitdekpe@gmail.com>
2024-03-30 09:24:32 +0100

    Plus de créneau libre pour cette date. Choisissez une date avec des créneaux libres affichés.

--- src/Form/PreRendezvousType.php
+++ src/Form/PreRendezvousType.php
@@ -51,7 +51,7 @@ class PreRendezvousType extends AbstractType
             'class' => Creneau::class,
             'choice_label' => 'libelle',
             'required'=> true,
-            'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'],
+            'attr' => ['class' => 'hidden bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'],
         ])
         ->add('supplement', EntityType::class, [
             'class' => Supplement::class,
